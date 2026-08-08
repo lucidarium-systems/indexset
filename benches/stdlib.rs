e5d6ab9cd291e49f9b00b77d1c8b5d6498e9549e
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2026-08-09 01:08:30 +0300

    migrate to Rust 2024 edition

--- benches/stdlib.rs
+++ benches/stdlib.rs
@@ -1,13 +1,12 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::seq::SliceRandom;
-use rand::thread_rng;
 use scc::TreeIndex;
 use std::hint::black_box;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let n = 100000;
     let mut input: Vec<usize> = (0..n).collect();
-    input.shuffle(&mut thread_rng());
+    input.shuffle(&mut rand::rng());
 
     c.bench_function("stdlib insert 100k", |b| {
         b.iter(|| {
